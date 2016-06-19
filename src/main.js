c20aa925cb3b375566de62c181efa15b2fb6717c
Clément Prod'homme <contact@clementprodhomme.fr>
2016-06-19 19:19:41 +0200

    Improves the dropdown menu by hiding it when clicking anywhere else

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ var mobileDrawer     = document.querySelector('.js-mobile-drawer');
 var menuDropdowns    = document.querySelectorAll('.js-menu-dropdown');
 var lazyLoadedImgs   = document.querySelectorAll('img[data-src]');
 
+/* Global variables */
+var openDropdowns = [];
+
 /* Methods*/
 function toggleMobileMenu() {
   mobileDrawer.classList.toggle('-opened');
@@ -11,24 +14,43 @@ function toggleMobileMenu() {
 
 function toggleDropdownMenu(e) {
   if(e.currentTarget !== e.target) return;
-  e.currentTarget.querySelector('ul').classList.toggle('-opened');
+  var dropdown = e.currentTarget.querySelector('ul');
+  var opened = dropdown.classList.toggle('-opened');
+
+  if(opened) {
+    openDropdowns.push(dropdown);
+    e.stopPropagation();
+  } else {
+    var index = openDropdowns.indexOf(dropdown);
+    if(!!~index) openDropdowns.splice(index, 1);
+  }
+}
+
+function closeDropdowns() {
+  openDropdowns.forEach(function(dropdown) {
+    dropdown.classList.remove('-opened');
+  });
+  openDropdowns = [];
 }
 
 function showImage(e) {
   var image = e.currentTarget;
   image.removeAttribute('data-src');
   image.classList.add('-loaded');
-  console.log('loaded!');
 }
 
 /* Event listeners */
 mobileMenuButton.addEventListener('click', toggleMobileMenu);
+
 Array.prototype.slice.call(menuDropdowns).forEach(function(menuDropdown) {
   menuDropdown.addEventListener('click', toggleDropdownMenu);
 });
+
 Array.prototype.slice.call(lazyLoadedImgs).forEach(function(lazyLoadedImg) {
   lazyLoadedImg.addEventListener('load', showImage);
 });
+
+document.body.addEventListener('click', closeDropdowns);
 
 /* General code */
 Array.prototype.slice.call(lazyLoadedImgs).forEach(function(lazyLoadedImg) {
