cf841a683b254962313ea8aed86bb193e2221ce5
Clément Prod'homme <contact@clementprodhomme.fr>
2016-06-19 18:53:43 +0200

    Adds a basic hero section without video

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 var mobileMenuButton = document.querySelector('.js-mobile-menu');
 var mobileDrawer     = document.querySelector('.js-mobile-drawer');
 var menuDropdowns    = document.querySelectorAll('.js-menu-dropdown');
+var lazyLoadedImgs   = document.querySelectorAll('img[data-src]');
 
 /* Methods*/
 function toggleMobileMenu() {
@@ -13,8 +14,23 @@ function toggleDropdownMenu(e) {
   e.currentTarget.querySelector('ul').classList.toggle('-opened');
 }
 
+function showImage(e) {
+  var image = e.currentTarget;
+  image.removeAttribute('data-src');
+  image.classList.add('-loaded');
+  console.log('loaded!');
+}
+
 /* Event listeners */
 mobileMenuButton.addEventListener('click', toggleMobileMenu);
 Array.prototype.slice.call(menuDropdowns).forEach(function(menuDropdown) {
   menuDropdown.addEventListener('click', toggleDropdownMenu);
+});
+Array.prototype.slice.call(lazyLoadedImgs).forEach(function(lazyLoadedImg) {
+  lazyLoadedImg.addEventListener('load', showImage);
+});
+
+/* General code */
+Array.prototype.slice.call(lazyLoadedImgs).forEach(function(lazyLoadedImg) {
+  lazyLoadedImg.src = lazyLoadedImg.getAttribute('data-src');
 });
